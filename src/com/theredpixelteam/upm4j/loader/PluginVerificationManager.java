447eaba96787828c62c398fe006523047ef14bd5
Kumonda221 <kumonda@kucro3.org>
2019-05-14 19:55:33 +0800

    增量更新 20190514

--- src/com/theredpixelteam/upm4j/loader/PluginVerificationManager.java
+++ src/com/theredpixelteam/upm4j/loader/PluginVerificationManager.java
@@ -1,5 +1,32 @@
 package com.theredpixelteam.upm4j.loader;
 
+import com.theredpixelteam.redtea.util.Optional;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+
 public class PluginVerificationManager {
     // TODO
+
+    public boolean addVerifier(@Nonnull PluginVerifier verifier)
+    {
+        return verifierMap.put(verifier.getIdentity(), verifier) != null;
+    }
+
+    public boolean removeVerifier(@Nonnull String identity)
+    {
+        return verifierMap.remove(Objects.requireNonNull(identity)) != null;
+    }
+
+    public @Nonnull Optional<PluginVerifier> getVerifier(@Nonnull String identity)
+    {
+        return Optional.ofNullable(verifierMap.get(Objects.requireNonNull(identity)));
+    }
+
+    public @Nonnull Collection<PluginVerifier> getAllVerifiers()
+    {
+        return Collections.unmodifiableCollection(verifierMap.values());
+    }
+
+    private final Map<String, PluginVerifier> verifierMap = new HashMap<>();
 }
